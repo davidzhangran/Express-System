b8b83bea7249f8bacccd514e597ac6289a529ba0
dengjinlong <122446636@qq.com>
2019-05-08 15:58:15 +0800

    新增门店查询

--- myapp/dao/storefrontDao.js
+++ myapp/dao/storefrontDao.js
@@ -19,12 +19,18 @@ module.exports.getStorefrontByPage = async (parm) => {
     let {
         currentPage,
         eachPage,
+        value,
+        inputText
     } = parm;
     let storefrontInfo = await storefrontModel
-        .find()
+        .find({
+            [value]: inputText
+        })
         .skip((currentPage - 1) * eachPage) //跳过多少个
         .limit(eachPage - 0) //查询多少个
-    let count = await storefrontModel.find();//总条数
+    let count = await storefrontModel.find({
+        [value]: inputText
+    });//总条数
     let totalPage = Math.ceil(count.length / eachPage); //总页数
     return {
         count: count.length,
@@ -34,4 +40,11 @@ module.exports.getStorefrontByPage = async (parm) => {
         storefrontInfo
     }
 
+}
+
+//修改门店
+module.exports.updateStorefront = async (parm) => {
+    console.log(parm);
+    let data = await storefrontModel.updateMany({ _id: parm._id }, parm);
+    return data;
 }
