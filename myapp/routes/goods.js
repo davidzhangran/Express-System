5d0306879b6b5574ef8432f5b21c9a0a5756efb1
suncong <904159787@qq.com>
2019-05-08 16:01:55 +0800

    商品后台完成

--- myapp/routes/goods.js
+++ myapp/routes/goods.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { uploadFile } = require("../util/upload.js");
-const { addCommodity,getcommoditysByPage } = require("../service/goodsSer.js");
+const { addCommodity, getcommoditysByPage, upDataCommodity, removeCommodity } = require("../service/goodsSer.js");
 /* GET home page. */
 router.post('/addCommodity', async function (req, res, next) {
   res.send(await addCommodity(req.body));
@@ -9,6 +9,14 @@ router.post('/addCommodity', async function (req, res, next) {
 router.get('/getcommoditysByPage', async function (req, res, next) {
   res.send(await getcommoditysByPage(req.query));
 });
+router.post('/upDataCommodity', async function (req, res, next) {
+  res.send(await upDataCommodity(req.body));
+});
+
+router.post('/removeCommodity', async function (req, res, next) {
+  res.send(await removeCommodity(req.body));
+});
+
 router.post('/addCommodityImg', async function (req, res, next) {
   let result = await uploadFile(req, res, {
     fileType: "imgags",//图片保存的文件名
